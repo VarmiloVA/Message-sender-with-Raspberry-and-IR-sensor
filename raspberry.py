38c58bcfbbe77e91daef240c1dcac959ff2c699b
Varmilo <90532063+VarmiloVA@users.noreply.github.com>
2022-05-04 17:06:48 +0200

    Funcionamiento del sensor infrarrojo implementado

--- raspberry.py
+++ raspberry.py
@@ -1,5 +1,4 @@
 #Libraries
-from collections import UserString
 from time import sleep
 import datetime
 import pyttsx3
@@ -8,14 +7,6 @@ import json
 #My own files
 import sensor
 from sms import *
-
-#Setting up the voice.
-engine = pyttsx3.init()
-volume = engine.getProperty('volume')
-engine.setProperty('volume', 0.5)
-speed = engine.getProperty('rate')
-#Optimal speed is -50
-engine.setProperty('rate', speed-50)
 
 class Raspberry:
     def __init__(self, flag):
@@ -53,83 +44,30 @@ class Raspberry:
         self.sensor_message()
 
     def sensor_message(self):
-        movement = self.sensor.check_movement()
-
-        if movement == 1:
             user = self.users['buzon1']
-            if user['activated'] == 0:
-                engine.say('Para comprobar tu correo primero has de dar de alta tu número de teléfono')
-                engine.runAndWait()
-                engine.stop()
-            else:
-                self._notify(user)
-        
-        elif movement == 2:
-            user = self.users['buzon2']
-            if user['activated'] == 0:
-                engine.say('Para comprobar tu correo primero has de dar de alta tu número de teléfono')
-                engine.runAndWait()
-                engine.stop()
-            else:
-                self._notify(user)
-        
-        elif movement == 3:
-            user = self.users['buzon3']
-            if user['activated'] == 0:
-                engine.say('Para comprobar tu correo primero has de dar de alta tu número de teléfono')
-                engine.runAndWait()
-                engine.stop()
-            else:
-                self._notify(user)
-        
-        elif movement == 4:
-            user = self.users['buzon4']
-            if user['activated'] == 0:
-                engine.say('Para comprobar tu correo primero has de dar de alta tu número de teléfono')
-                engine.runAndWait()
-                engine.stop()
-            else:
-                self._notify(user)
-        
-        elif movement == 5:
-            user = self.users['buzon5']
-            if user['activated'] == 0:
-                engine.say('Para comprobar tu correo primero has de dar de alta tu número de teléfono')
-                engine.runAndWait()
-                engine.stop()
-            else:
-                self._notify(user)
-
-        else:
-            engine.say('Aquí se encuentra el fallo')
-            engine.runAndWait()
-            engine.stop()
+            self._notify(user)
 
     def _notify(self, user):
             message = f'Ha llegado correo al buzón de {user["owner"]}'
             print(message)
-            engine.say(message)
-            engine.runAndWait()
-            engine.stop()
 
             if self.flag != 1:
                 sms_send('Ha llegado correo al buzón', user['phone_number'])
 
-flag = 0
-while __name__ == '__main__':
-    """The program will work only if it's ejecuted as main"""
-    rb = Raspberry(flag)
+k = sensor.Sensor()
+movement = k.check_movement()
+while movement:
+    flag = 0
+    while __name__ == '__main__':
+        """El programa solo va a funcionar si es ejecutado como principal"""
+        rb = Raspberry(flag)
 
-    if flag != 2:
-        rb.run()
-        flag += 1
-        sleep(1.7)
-    elif flag == 2:
-        engine.say('Fin del mensaje')
-        engine.runAndWait()
-        engine.stop()
+        if flag != 2:
+            rb.run()
+            flag += 1
+            sleep(1.5)
+        elif flag == 2:
         
-        if rb.sensor.check_movement() != False:
-            break
-        else:
-            sleep(15)
+            if rb.sensor.check_movement() != False:
+                break
+    break
--- sensor.py
+++ sensor.py
@@ -1,32 +1,11 @@
+from gpiozero import MotionSensor
+
 class Sensor:
     def __init__(self):
-        self.mailbox1 = False
-        self.mailbox2 = False
-        self.mailbox3 = False
-        self.mailbox4 = False
-        self.mailbox5 = False
+        self.mailbox = False
+        self.pir = MotionSensor(23)
 
     def check_movement(self):
         """Check de movement status"""
-        self.mailbox1 = True
-        self.mailbox2 = False
-        self.mailbox3 = False
-        self.mailbox4 = False
-        self.mailbox5 = False
-
-        mailboxes = [self.mailbox1, self.mailbox2, self.mailbox3, self.mailbox4, self.mailbox5]
-        for i in mailboxes:
-            if i == True:
-                if i == self.mailbox1:
-                    return 1
-                if i == self.mailbox2:
-                    return 2 
-                if i == self.mailbox3:
-                    return 3
-                if i == self.mailbox4:
-                    return 4
-                if i == self.mailbox5:
-                    return 5
-            
-            else:
-                return False
+        self.pir.wait_for_motion()
+        return True
