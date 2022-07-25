2e5738255e7048011dd2f8523c4ac70d07374bda
Daniel Atanasov <varmilo.blue@protonmail.com>
2022-07-25 09:52:32 +0200

    virtual env created

--- src/sms.py
+++ src/sms.py
@@ -5,12 +5,12 @@ def sms_send(text, phone_number):
     #Código sacado de la propia documentación de twilio
     #Hay que dar de alta el número al que se quiera manda el SMS en: https://bit.ly/3k2GcLI.
  
-    account_sid = 'ACef0867a31b82b7fcc145855083dc6477' 
-    auth_token = '5dd20f4c22df3e9d06154762faad39ef' 
+    account_sid = '*****************************' 
+    auth_token = '*****************************' 
     client = Client(account_sid, auth_token) 
  
     message = client.messages.create(  
-        messaging_service_sid='MGbda50f472a898a67b63118b29fc05c45', 
+        messaging_service_sid='*************************', 
         body=text,      
         to=phone_number
     )
